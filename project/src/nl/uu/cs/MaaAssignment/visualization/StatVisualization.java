c965cd73bb21e545771031fe03e71563f51292a0
aelex13 <du.a.meijer@googlemail.com>
2017-05-12 19:03:30 +0200

    Initial test to get charts working

--- project/src/nl/uu/cs/MaaAssignment/visualization/StatVisualization.java
+++ project/src/nl/uu/cs/MaaAssignment/visualization/StatVisualization.java
@@ -2,6 +2,14 @@ package nl.uu.cs.MaaAssignment.visualization;
 
 import nl.uu.cs.MaaAssignment.IObserver;
 import nl.uu.cs.MaaAssignment.Simulation;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.SeriesException;
+import org.jfree.data.time.Second;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
 
 import javax.swing.*;
 
@@ -11,10 +19,19 @@ import javax.swing.*;
 public class StatVisualization extends JPanel implements IObserver {
 
     private Simulation simulation;
+    private JFreeChart chart;
+    private XYDataset dataset;
+    private ChartPanel chartPanel;
 
     public StatVisualization(Simulation simulation){
         super();
         this.registerSimulation(simulation);
+
+        this.dataset = this.createDataset();
+        this.chart = this.createChart(dataset);
+        this.chartPanel = new ChartPanel(chart);
+        this.add(chartPanel);
+
     }
 
     private void registerSimulation(Simulation simulation){
@@ -25,5 +42,41 @@ public class StatVisualization extends JPanel implements IObserver {
     @Override
     public void update() {
 
+    }
+
+    private JFreeChart createChart( final XYDataset dataset ) {
+        return ChartFactory.createTimeSeriesChart(
+                "Mean reward of action per time step",
+                "Time-step",
+                "Reward",
+                dataset,
+                false,
+                false,
+                false);
+    }
+
+    private XYDataset createDataset( ) {
+        TimeSeriesCollection toReturn = new TimeSeriesCollection();
+
+        for (int i = 0; i < 2; i++){
+
+            TimeSeries series = new TimeSeries( "Random Data" + i );
+            Second current = new Second( );
+            double value = 100.0;
+
+            for (int j = 0; j < 4000; j++) {
+
+                try {
+                    value = value + Math.random( ) - 0.5;
+                    series.add(current, new Double( value ) );
+                    current = (Second) current.next( );
+                } catch ( SeriesException e ) {
+                    System.err.println("Error adding to series");
+                }
+            }
+            toReturn.addSeries(series);
+        }
+
+        return toReturn;
     }
 }
