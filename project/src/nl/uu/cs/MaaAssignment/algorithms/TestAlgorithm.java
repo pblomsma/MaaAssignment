ec55044c28e0081f57cc6607932c4f8d3697c9ac
Peter Blomsma <peter.blomsma@gmail.com>
2017-05-13 11:55:20 +0200

    Test algorithm was only choosing action 1.. resulted in a boring graph.

--- project/src/nl/uu/cs/MaaAssignment/algorithms/TestAlgorithm.java
+++ project/src/nl/uu/cs/MaaAssignment/algorithms/TestAlgorithm.java
@@ -1,12 +1,15 @@
 package nl.uu.cs.MaaAssignment.algorithms;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Peter on 9-5-2017.
  */
 public class TestAlgorithm implements Algorithm
 {
+    List<Double> _actions;
 
     public void initialize(Object[] parameters) {
 
@@ -15,12 +18,15 @@ public class TestAlgorithm implements Algorithm
     @Override
     public Algorithm initialize(List<Double> actions, Object[] parameters)
     {
+        _actions = actions;
         return this;
     }
 
     @Override
     public int nextAction(int round) {
-        return 1;
+        //random choice
+        Random random = new Random();
+        return random.nextInt(_actions.size());
     }
 
     @Override
