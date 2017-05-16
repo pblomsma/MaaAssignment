fecab02cacb31968fc6f3d4bf7686d4f1b3c539e
Peter Blomsma <peter.blomsma@gmail.com>
2017-05-16 15:27:42 +0200

    Added extra tests to feel a bit more secure about the code ;)

--- project/test/nl/uu/cs/MaaAssignment/CollisionDetectionTorusTest.java
+++ project/test/nl/uu/cs/MaaAssignment/CollisionDetectionTorusTest.java
@@ -14,7 +14,7 @@ public class CollisionDetectionTorusTest {
         //Above
         assertEquals(1, CollisionDetectionTorus.distanceMovingAgent(2,3,1,2,3,2,5,5), 0.01);
     }
-
+    @Test
     public void distanceMovingAgentTorusHorizontal() throws Exception
     {
         //Below
@@ -23,4 +23,13 @@ public class CollisionDetectionTorusTest {
         //Above
         assertEquals(2, CollisionDetectionTorus.distanceMovingAgent(2,1,1,4,3,4,5,5), 0.01);
     }
+
+    @Test
+    public void distanceMovingAgentTorusHorizontalVar1() throws Exception
+    {
+        //Advanced one
+        assertEquals(Math.sqrt(2), CollisionDetectionTorus.distanceMovingAgent(18   ,18,17,1,1,17,20,20), 0.01);
+        assertEquals(Math.sqrt(2), CollisionDetectionTorus.distanceMovingAgent(19   ,19,19,1,1,19,20,20), 0.01);
+    }
+
 }
